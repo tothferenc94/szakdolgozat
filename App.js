94625b58346a2d8b9a6eea014fddc24bfc030f4e
Tóth Ferenc <tothfo@gmail.com>
2020-02-14 15:38:44 +0100

    navigation

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ import {Router, Scene} from 'react-native-router-flux';
 export default class App extends Component {
   render() {
     return(
-     <Welcome/>
+    <Router>
+      <Scene key="root">
+        <Scene
+          key="fokepernyo"
+          title="kezdokepernyo"
+          component={Welcome}
+          initial
+        />
+      </Scene>
+    </Router>
     );
   }
 }
